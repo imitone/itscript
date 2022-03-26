930214f42e7ce38378d5a500301cf366987e6134
imitone <95508921+imitone@users.noreply.github.com>
2022-03-26 13:15:44 +0300

    Add files via upload

---
+++ it.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+echo "Welcome To iT Shell <3"
+
+sleep 3
+
+echo "I will Install Some Programs In Your System"
+
+sleep 3
+
+echo "Let's Starting Don't Be Scared <3 "
+
+sleep 3
+
+echo "Now We Will Update Your System"
+
+sleep 3
+
+sudo apt update
+
+echo "Now We Will Upgrade Your System"
+
+sleep 3
+
+sudo apt upgrade -y 
+
+echo "Thank You Add Me On Discord iT#0001"
